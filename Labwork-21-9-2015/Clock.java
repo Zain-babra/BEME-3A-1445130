854995995290cab4149bb729b5960b4352659994
Zain-babra <z.a.babra@gmail.com>
2015-09-21 17:27:28 +0500

    Create Clock.java

---
+++ Labwork-21-9-2015/Clock.java
@@ -0,0 +1,37 @@
+package Clock;
+
+public class Clock 
+{
+	private Counter hours,mins,secs;
+	public Clock()
+	{
+		hours = new Counter(24);
+		mins = new Counter(60);
+		secs = new Counter(60);
+		
+	}
+		public void tick()
+		{
+			secs.increment ();
+			if(secs.viewCount()==0)
+			{
+				mins.increment();
+				if((secs.viewCount()==0) && (mins.viewCount()==0))
+					hours.increment();
+			}
+			
+		}
+		public int viewHr( )
+		{
+			return hours.viewCount();
+		}
+		public int viewMin()
+		{
+			return mins.viewCount();
+		}
+		public void ShowTime()
+		{
+			System.out.println(hours.viewCount()+":"+mins.viewCount()+":"+secs.viewCount());
+		}
+	
+}
